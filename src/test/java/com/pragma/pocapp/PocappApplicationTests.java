38459e5586c29c3d71e4749effed7f8eb8f1f199
jose.osorio <jose.osorio@pragma.com.co>
2022-02-07 19:09:34 -0500

    Initial commit

---
+++ src/test/java/com/pragma/pocapp/PocappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.pocapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
